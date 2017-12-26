9a27950bdff9f4e44e35e15ecf8c85526d92c437
hilyd <danjean.lydie@gmail.com>
2017-12-26 20:28:40 +0100

    section admin et section admin create et read

---
+++ templates/part/section-admin.php
@@ -0,0 +1,25 @@
+<?php
+
+$verifMembre = $objetSession->get("membre");
+
+?>
+
+<section>
+    <h3>SECTION ADMIN</h3>
+    <h4>BIENVENUE <?php echo $verifMembre ?></h4>
+
+
+<?php 
+
+// if ($objetRequest->get("afficher", "") == "update")
+// {
+//     require_once("$cheminPart/admin-article-update.php");
+// }
+// else
+// {
+    require_once("$cheminPart/admin-article-create.php");
+// }
+
+    require_once("$cheminPart/admin-article-read.php");
+?>
+</section>
