7d7ee555204e6de8c2c56cc02563c936428c51f8
Molodit <mmolodit@yahoo.com>
2018-01-04 11:39:20 +0100

    section-journal et section-rhizome

---
+++ templates/part/section-journal.php
@@ -0,0 +1,64 @@
+
+
+<section>
+
+    <h3>LISTE DES ARTICLES DE CREATEXTE</h3> 
+
+<?php
+// ALLER CHERCHER LA LISTE DES ARTICLES DANS LA CATEGORIE $createxte
+
+// JE VAIS RECUPERER LE REPOSITORY POUR L'ENTITE Article
+$objetRepository = $this->getDoctrine()->getRepository(App\Entity\MonArticle::class);
+
+
+// ATTENTION: ON UTILISE LE NOM DES PROPRIETES
+$tabResultat = $objetRepository->findBy([ "rubrique" => "Journal" ], [ "datePublication" => "DESC" ]);
+
+// ON A UN TABLEAU D'OBJETS DE CLASSE Article
+foreach($tabResultat as $objetArticle)
+{
+    // METHODES "GETTER" A RAJOUTER DANS LA CLASSE MonArticle
+    $idArticle       = $objetArticle->getIdArticle();
+    $titre           = $objetArticle->getTitre();
+    $contenu         = $objetArticle->getContenu();
+    $rubrique        = $objetArticle->getRubrique();
+    $motCle          = $objetArticle->getMotCle();
+    $cheminImage     = $objetArticle->getCheminImage();
+    $datePublication = $objetArticle->getDatePublication("d/m/Y H:i:s");
+    
+    
+    $htmlImage = "";
+    if ($cheminImage)
+    {
+        $htmlImage = 
+<<<CODEHTML
+
+    <img src="$urlAccueil/$cheminImage" title="$cheminImage">
+
+CODEHTML;
+    }
+    
+    $urlJournal         = $this->generateUrl("rubrique", [ "rub" => "Journal" ]);
+    // CREER L'URL POUR LA ROUTE DYNAMIQUE (AVEC PARAMETRE)
+    $urlArticle = $this->generateUrl("article", [ "id_article" => $idArticle ]);
+    
+    echo
+<<<CODEHTML
+
+    <article>
+    <td>$idArticle</td>
+        <h4 title="$idArticle"><a href="$urlArticle">$titre</a></h4>
+        <div>$rubrique</div>
+        <p>$contenu</p>
+        <div>$htmlImage</div>
+        <div>$datePublication</div>
+    </article>
+    
+CODEHTML;
+    
+}
+
+?>
+
+
+</section>
