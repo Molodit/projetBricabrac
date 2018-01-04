268ab66694f0f6883a5230d60214e2c928243608
Mariina13 <34568992+Mariina13@users.noreply.github.com>
2018-01-04 11:28:46 +0100

    modif

--- templates/template-accueil-enfant.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once("$cheminPart/header.php");
-require_once("$cheminPart/section-accueil-enfant.php");
-require_once("$cheminPart/section-enfant-read.php");
-require_once("$cheminPart/footer.php");
--- templates/template-admin-enfant.php
+++ templates/template-admin-enfant.php
@@ -2,5 +2,4 @@
 
 require_once("$cheminPart/header.php");
 require_once("$cheminPart/section-admin-enfant.php");
-require_once("$cheminPart/section-admin-enfant-read.php");
 require_once("$cheminPart/footer.php");
