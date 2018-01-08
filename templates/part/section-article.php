56902e2986ee7efad13e4c30627e8f4bad5d402c
Mariina13 <34568992+Mariina13@users.noreply.github.com>
2018-01-08 14:48:16 +0100

    ajout du formulaire de commentaire + fichier complémentaire

--- templates/part/section-article.php
+++ templates/part/section-article.php
@@ -50,5 +50,31 @@ CODEHTML;
 }
 
 ?>
-
 </section>
+
+<section id="commentaire">
+    <h3>Laisser un Commentaire : </h3>
+    
+    <form method="POST">
+        <input type="text" name="titre" required placeholder="Votre Titre"/>
+            <textarea id="editor1" type="text" name="contenu" required placeholder="contenu" rows="30"></textarea>
+            <button type="submit">Ajouter votre commentaire <i class="far fa-hand-point-right"></i> </button>
+        <input type="hidden" name="codebarre" value="commentaire">
+        <div class="response">
+<?php
+// TRAITER LE FORMULAIRE
+if ($objetRequest->get("codebarre", "") == "commentaire")
+{
+    $objetFormCommentaire = new App\Controller\FormCommentaire;
+    
+    $objetFormCommentaire->creerCommentaire($objetRequest, $objetConnection, $cheminSymfony, $objetSession);
+
+
+}
+?>
+        </div>
+    </form>
+    
+    
+</section>
+
