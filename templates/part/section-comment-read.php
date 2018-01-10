b675b3ff8c70c06a7ce3d2d7606480e24a742832
Mariina13 <34568992+Mariina13@users.noreply.github.com>
2018-01-10 12:32:51 +0100

    read des commentaires + reglages dde l'apparition du formulaire des commentaires celon le niveau

---
+++ templates/part/section-comment-read.php
@@ -0,0 +1,37 @@
+<h3> Commentaires</h3>
+<section class="read">
+
+<?php
+
+// JE VAIS RECUPERER LE REPOSITORY POUR L'ENTITE Comments
+$objetRepository = $this->getDoctrine()->getManager()->getRepository(App\Entity\Comments::class);
+
+$tabResultat = $objetRepository->findBy([], [ "datePublication" => "DESC" ]);
+
+
+foreach($tabResultat as $objetComment)
+{
+
+    $idComment       = $objetComment->getIdComment();
+    $idMembre         = $objetComment->getIdMembre();
+    $contenu            = $objetComment->getContenu();
+
+// PLUS PRATIQUE => findBy
+// http://www.doctrine-project.org/api/orm/2.5/class-Doctrine.ORM.EntityRepository.html
+// ATTENTION: ON UTILISE LE NOM DES PROPRIETES
+$urlArticle = $this->generateUrl("article",["id_article" => "$idArticle"]);
+
+    echo
+    <<<CODEHTML
+<section>
+    <p>$idMembre</p>
+    <p>$contenu</p>
+</section>
+
+CODEHTML;
+
+}
+
+?>
+
+</section>
