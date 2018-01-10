0bdd5b0553df4c837ab1dc80d68a29def844a3c3
Hilyd <danjean.lydie@gmail.com>
2018-01-11 00:04:30 +0100

    section accueil

--- templates/part/section-accueil.php
+++ templates/part/section-accueil.php
@@ -1,73 +1,27 @@
-<section class="bgimg-1" id="accueil-articles">
+<section class="bgimg-1" id="accueil-balles">
 
 <?php
-// ALLER CHERCHER LA LISTE DES ARTICLES DANS LA CATEGORIE $cat
-
 // JE VAIS RECUPERER LE REPOSITORY POUR L'ENTITE Article
 // $objetRepository = $this->getDoctrine()->getRepository("App\Entity\MonArticle");
 $objetRepository     = $this->getDoctrine()->getRepository(App\Entity\MonArticle::class);
 
-
-$tabResultat = $objetRepository->trouverArticleUserLimit($objetConnection);
+$tabResultatMotCle = $objetRepository->trouverMotClePlus($objetConnection);
 $compteur = 0;
 // ON A UN TABLEAU D'OBJETS DE CLASSE Article
-foreach($tabResultat as $tabLigne)
+foreach($tabResultatMotCle as $tabLigneMotCle)
 {
     $compteur++;
-    extract($tabLigne);
+    extract($tabLigneMotCle);
 
-    $htmlFile = "";
-    if ($chemin_image)
-    {
-        $htmlFile = 
-<<<CODEHTML
+    if ($compteur < 12) {
 
-    <img class="balle" src="$chemin_image" alt="photo">
+$urlMotCle = $this->generateUrl("motCle", [ "mot_cle" => $mot_cle ]);
 
-CODEHTML;
-    }
-    
-    // CREER L'URL POUR LA ROUTE DYNAMIQUE (AVEC PARAMETRE)
-    $urlArticle     = $this->generateUrl("article", [ "id_article" => $idArticle ]);
-    if ($compteur < 4) {
     echo
 <<<CODEHTML
 <div class="circles-container container">
 
                   <div class="circle-link circle$compteur" >
-                  <div class="circle-content" >
-    <article style="background-image:url('$chemin_image');
-    background-repeat:no-repeat;
-    background-position:center;
-    background-size: cover;">
-       <a href="$urlArticle">$titre</a>
-        <p>de $membre</p>
-    </article>
-    </div>
-</div>
-    
-CODEHTML;
-    }
-    
-}
-
-$tabResultatMotCle = $objetRepository->trouverMotClePlus($objetConnection);
-$compteurMotCle = 3;
-// ON A UN TABLEAU D'OBJETS DE CLASSE Article
-foreach($tabResultatMotCle as $tabLigneMotCle)
-{
-    $compteurMotCle++;
-    extract($tabLigneMotCle);
-
-    if ($compteurMotCle > 3 && $compteurMotCle < 12) {
-
-$urlMotCle            = $this->generateUrl("motCle", [ "mot_cle" => $mot_cle ]);
-
-    echo
-<<<CODEHTML
-<div class="circles-container container">
-
-                  <div class="circle-link circle$compteurMotCle" >
                   <div class="circle-content" >
     <article>
         <a href="$urlMotCle">$mot_cle</a>
@@ -80,6 +34,74 @@ CODEHTML;
     
 }
 
+?>
+</section>
+
+<section id="accueil-articles">
+    <h2>Les derniers articles</h2>
+<?php
+
+$tabResultat = $objetRepository->trouverArticleUserLimit($objetConnection);
+
+// ON A UN TABLEAU D'OBJETS DE CLASSE Article
+foreach($tabResultat as $tabLigne)
+{
+    extract($tabLigne);
+     // CREER L'URL POUR LA ROUTE DYNAMIQUE (AVEC PARAMETRE)
+    $urlArticle     = $this->generateUrl("article", [ "id_article" => $idArticle ]);
+  
+    if ($chemin_image)
+    {
+        $objetExtension = new SplFileInfo($chemin_image);
+        $extension = $objetExtension->getExtension();
+
+        // Si le fichier est un pdf
+        if ($extension == "pdf")
+    {
+       echo
+        <<<CODEHTML
+        <article>
+            <div>
+                <a href="$urlArticle">$titre</a>
+                <iframe src="$chemin_image" width="100%"></iframe>
+                
+                <p>écrit par $membre</p>
+            </div>
+        </article>
+CODEHTML;
+    }
+
+    else {
+       
+        echo
+        <<<CODEHTML
+            <article style="background-image:url('$chemin_image');
+                background-repeat:no-repeat;
+                background-position:center;
+                background-size: cover;">
+                    <div>
+                        <a href="$urlArticle">$titre</a>
+                        <p>écrit par $membre</p>
+                    </div>
+            </article>
+CODEHTML;
+        }
+
+    }
+
+    else {
+        echo
+        <<<CODEHTML
+        <article>
+            <div>
+                <a href="$urlArticle">$titre</a>
+                <p>écrit par $membre</p>
+            </div>
+        </article>
+CODEHTML;
+    }
+    
+}
 ?>
 </section>
 
