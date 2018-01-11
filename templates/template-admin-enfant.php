4fcc2e291edd8b707af717b5dfd03a2794ab7ce8
Mariina13 <34568992+Mariina13@users.noreply.github.com>
2018-01-11 11:28:22 +0100

    !!

--- templates/template-admin-enfant.php
+++ templates/template-admin-enfant.php
@@ -2,5 +2,5 @@
 
 require_once("$cheminPart/header.php");
 require_once("$cheminPart/section-admin-enfant.php");
-require_once("$cheminPart/footer-enfant.php");
+require_once("$cheminPart/footer.php");
 
