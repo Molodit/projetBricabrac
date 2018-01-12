869d68d8021c250be1336cc80e2052f234435f0a
Hilyd <danjean.lydie@gmail.com>
2018-01-12 12:32:16 +0100

    class dans admin create

--- templates/part/footerCkEditor.php
+++ templates/part/footerCkEditor.php
@@ -13,7 +13,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
-   
+    <script src="https://cdn.ckeditor.com/4.8.0/standard-all/ckeditor.js"></script>
     <script type="text/javascript" src="<?php echo $urlAccueil ?>assets/js/dataTablesScript.js"></script>
     <script type="text/javascript" src="<?php echo $urlAccueil ?>assets/js/script.js"></script>
     <script type="text/javascript" src="<?php echo $urlAccueil ?>assets/js/ckEditor.js"></script>
