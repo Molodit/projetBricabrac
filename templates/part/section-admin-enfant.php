0f41812ab2b737a875e8ea0e9dfa340bbfc5caf5
Mariina13 <34568992+Mariina13@users.noreply.github.com>
2018-01-12 12:10:37 +0100

    css enfant+ formulaire

--- templates/part/section-admin-enfant.php
+++ templates/part/section-admin-enfant.php
@@ -2,17 +2,21 @@
 $verifMembre = $objetSession->get("membre");
 
 ?>
- <h2>BIENVENUE <?php echo $verifMembre ?></h2>
+ 
 
- <hr>
+ 
 
 <section id="formulaireEnfant">
+
+<h2>BIENVENUE <?php echo $verifMembre ?></h2>
+
     <h3>Ecrire un nouvel article</h3>
     
-    
+ <hr>    
+
     <!-- NE PAS OUBLIER: POUR UPLOADER UN FICHIER -->
     <!-- method="POST" enctype="multipart/form-data" -->
-    <form method="POST" enctype="multipart/form-data">
+    <form method="POST" enctype="multipart/form-data" class="formEnfant">
         <input type="text" name="titre" required placeholder="titre">
         <input type="text" name="mot_cle" required placeholder="mot-clé">
         <select name="rubrique" required>
@@ -21,10 +25,13 @@ $verifMembre = $objetSession->get("membre");
             <option value="CréaTexte">Créatexte</option>
             <option value="Journal">Journal La Tanière</option>
         </select>
-        <textarea id="editor1" type="text" name="contenu" required placeholder="contenu" rows="30"></textarea>
+        <textarea id="editor1" type="text" name="contenu" required placeholder="contenu" rows="30">    
+        </textarea>
         <input type="file" name="cheminImage" placeholder="insérer une image">
         <button type="submit">Publier l'article</button>
-        <input type="hidden" name="codebarre" value="article">
+        <button type="submit">Enregistrer en brouillon</button>
+        <input type="hidden" name="statut" value="brouillon">
+        <input type="hidden" name="statut" value="article">
         <div class="response">
 <?php
 // TRAITER LE FORMULAIRE
