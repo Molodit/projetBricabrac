697f0a86cecb0b43f1bc27d65e16c030ee6a24ab
Hilyd <danjean.lydie@gmail.com>
2018-01-16 10:45:46 +0100

    header

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,30 +1,36 @@
-var target = document.querySelector('.main');
-var button = document.querySelector('.nav-button');
+// var target = document.querySelector('.main');
+// var button = document.querySelector('.nav-button');
 
-// click-touch event
-(function() {
+// // click-touch event
+// (function() {
 
-    // old browser or not ?
-    if ( !('querySelector' in document && 'addEventListener' in window) ) {
-    return;
-    }
-    window.document.documentElement.className += ' js-enabled';
+//     // old browser or not ?
+//     if ( !('querySelector' in document && 'addEventListener' in window) ) {
+//     return;
+//     }
+//     window.document.documentElement.className += ' js-enabled';
     
-    function toggleNav() {
+//     function toggleNav() {
     
-    // Define targets
-    var target = document.querySelector('.main');
-    var button = document.querySelector('.nav-button');
+//     // Define targets
+//     var target = document.querySelector('.main');
+//     var button = document.querySelector('.nav-button');
     
-    // click-touch event
-    if ( button ) {
-      button.addEventListener('click', 
-      function (e) { 
-        target.classList.toggle('is-opened'); 
-        e.preventDefault();
-      }, false );
-    }
-    } // end toggleNav()
+//     // click-touch event
+//     if ( button ) {
+//       button.addEventListener('click', 
+//       function (e) { 
+//         target.classList.toggle('is-opened'); 
+//         e.preventDefault();
+//       }, false );
+//     }
+//     } // end toggleNav()
     
-    toggleNav();
-    }());
+//     toggleNav();
+//     }());
+
+jQuery(function($){
+    $( '.menu-btn' ).click(function(){
+    $('.responsive-menu').toggleClass('expand')
+    })
+})
