8b29c9b8240961d09bce12b18e4ab8d031b6fa88
Mariina13 <34568992+Mariina13@users.noreply.github.com>
2018-01-31 10:03:08 +0100

    mise en place multiupload dans section enfant

--- templates/part/section-enfant-create.php
+++ templates/part/section-enfant-create.php
@@ -31,14 +31,68 @@ $verifMembre = $objetSession->get("membre");
       <option value="Rhizome">Rhizome</option>
       <option value="CreaTexte">CréaTexte</option>
       <option value="Journal">Journal La Tanière</option>
- </select>
+    </select>
     <textarea id="editor1" type="text" name="contenu" required placeholder="Votre texte" rows="30"></textarea>
-    <input type="file" name="cheminImage" placeholder="Insérer une image">
-    <!-- Deux boutons pour donner la possibilité d'enregistrer en brouillon -->
-    <button type="submit" name="statut" value="publie">Publier l'article</button>
-    <button type="submit" name="statut" value="brouillon">Enregistrer l'article</button>
-    <input type="hidden" name="codebarre" value="article">
-    <div class="response">
+    <input type="hidden" name="cheminImage">
+        <!-- Deux boutons pour donner la possibilité d'enregistrer en brouillon -->
+        <div id="filelist"></div>
+                <br />
+                
+                <div id="container">
+                    <a id="pickfiles" href="javascript:;">[Select files]</a> 
+                    <a id="uploadfiles" href="javascript:;">[Upload files]</a>
+                </div>
+                
+                <br />
+                <pre id="console"></pre>
+        <button type="submit" name="statut" value="publie">Publier l'article</button>
+        <button type="submit" name="statut" value="brouillon">Enregistrer comme brouillon</button>
+        <input type="hidden" name="codebarre" value="article">
+        <div class="response">
+        
+<script type="text/javascript">
+ 
+var uploader = new plupload.Uploader({
+  runtimes : 'html5,flash,silverlight,html4',
+  browse_button : 'pickfiles',
+  container: document.getElementById('container'), 
+  url : "../assets/upload/upload.php",
+
+  
+  filters : {
+    max_file_size : '10mb',
+    mime_types: [
+      {title : "Image files", extensions : "jpg,gif,png,jpeg"},
+      {title : "Zip files", extensions : "zip"}
+    ]
+  },
+  init: {
+    PostInit: function() {
+      document.getElementById('filelist').innerHTML = '';
+      document.getElementById('uploadfiles').onclick = function() {
+        uploader.start();
+        return false;
+      };
+    },
+    FilesAdded: function(up, files) {
+      plupload.each(files, function(file) {
+          document.querySelector("input[name=cheminImage]").value += file.name + ",";
+        document.getElementById('filelist').innerHTML += '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
+      });
+    },
+    UploadProgress: function(up, file) {
+      document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
+    },
+    Error: function(up, err) {
+      document.getElementById('console').appendChild(document.createTextNode("\nError #" + err.code + ": " + err.message));
+    }
+  }
+});
+uploader.init();
+
+          
+          
+</script>         
     
 <?php
 // TRAITER LE FORMULAIRE
