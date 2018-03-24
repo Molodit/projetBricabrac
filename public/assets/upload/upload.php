83361665de59bc17363cc35df55d987f275ce9c0
Marina <mmolodit@yahoo.com>
2018-03-24 17:39:42 +0100

    upload

--- public/assets/upload/upload.php
+++ public/assets/upload/upload.php
@@ -39,9 +39,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 // Settings
 $targetDir = ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";
+//$targetDir = ini_get("upload_tmp_dir") . "plupload";
 
+//$targetDir = 'public' . DIRECTORY_SEPARATOR . 'upload';
 //$targetDir = __DIR__ ;
-$targetDir = "../upload/";
+$targetDir = "__DIR__/upload/";
 $cleanupTargetDir = true; // Remove old files
 $maxFileAge = 5 * 3600; // Temp file age in seconds
 
--- templates/part/admin-article-create.php
+++ templates/part/admin-article-create.php
@@ -71,7 +71,7 @@ if ($objetRequest->get("codebarre", "") == "article")
 $(function() {
     $("#uploader").plupload({
         // General settings
-        runtimes : 'html5,flash,silverlight,html4',
+        runtimes : 'html5,gears,flash,silverlight,browserplus,html4',
         url : "../assets/upload/upload.php",
        
  
@@ -120,9 +120,9 @@ $(function() {
       var inputName = $( "input[name*='_name']" );
   
       inputName.each(function(){
-         cheminImage += 'assets/upload/' + $(this).val() + ',';
+         cheminImage += 'assets/upload/' + $(this).val() + ','
         
-      })
+      });
      
     $("input[name=cheminImage]").val(cheminImage);
           });
