93b01b7f95007b4094a17c792136cede3e295067
Hilyd <danjean.lydie@gmail.com>
2018-04-13 15:32:35 +0200

    commentaire dans slider.js

--- public/assets/js/slider.js
+++ public/assets/js/slider.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+  // Slider sur section article
   $('.article-commentaire .flexslider').flexslider({
     animation: "slide",
     controlNav: "thumbnails"
   });
 
+  // Slider sur section rhizome
   $('.article-rhizome .flexslider').flexslider({
     animation: "slide"
   });
